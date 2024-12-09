fa990ceff320f8c20469524793c841f1b46ba8ea
John Puka <johnpuka03@gmail.com>
2024-12-09 16:34:52 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -142,5 +142,5 @@ app.post('/send-email', async (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
